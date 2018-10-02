273af1e57be5f1fdc255b7e0314b5f9e967e30a1
Kadu-jr <kadu.jr95@gmail.com>
2018-10-02 00:59:26 -0300

    Aumentando limite do campo de codigo do template

--- db/schema.rb
+++ db/schema.rb
@@ -239,7 +239,7 @@ ActiveRecord::Schema.define(version: 20180901230532) do
   create_table "form_templates", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.string "code"
+    t.string "code" , limit: 4294967295
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
