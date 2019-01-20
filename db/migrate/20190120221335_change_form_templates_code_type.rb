5d2e836fe7fd84d9f6bbbc2719a37db8abc6d654
Ubuntu <ubuntu@ip-172-31-28-154.us-west-2.compute.internal>
2019-01-20 22:25:16 +0000

    Correcao na modificacao do campo code de Templates

--- db/migrate/20190120221335_change_form_templates_code_type.rb
+++ db/migrate/20190120221335_change_form_templates_code_type.rb
@@ -1,5 +1,5 @@
 class ChangeFormTemplatesCodeType < ActiveRecord::Migration[5.1]
   def up
-    change_column :FormTemplate, :code, :text
+    change_column :form_templates, :code, :text
   end
 end
